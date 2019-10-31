bbd9d4f79cf719eaf7529167f5d2192763d40eed
rdeyvil <rdeyvil11@gmail.com>
2019-10-31 12:20:32 +0545

    Adds emacs config file

---
+++ .emacs.d/init.el
@@ -0,0 +1,13 @@
+(setq inhabit-startuo-message)
+
+(require 'use-package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa"."https://melpa.org/packages/") )
+
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package) )
